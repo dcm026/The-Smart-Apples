4bd06ad2d8f96895d89e74377e3685564ec832e6
Blake Till <jbt017@gmail.com>
2021-02-23 19:08:26 -0600

    rebuilt unit tests default

---
+++ proj4Tests/proj4Tests.swift
@@ -0,0 +1,33 @@
+//
+//  proj4Tests.swift
+//  proj4Tests
+//
+//  Created by Katie Till on 2/23/21.
+//  Copyright © 2021 Sam Spohn. All rights reserved.
+//
+
+import XCTest
+
+class proj4Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
