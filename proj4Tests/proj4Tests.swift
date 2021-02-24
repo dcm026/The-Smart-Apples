7d54581fe1807477c191b21c36c8e19556bfcad6
Blake Till <jbt017@gmail.com>
2021-02-24 00:30:49 -0600

    unit test

--- proj4Tests/proj4Tests.swift
+++ proj4Tests/proj4Tests.swift
@@ -2,25 +2,34 @@
 //  proj4Tests.swift
 //  proj4Tests
 //
-//  Created by Blake Till on 2/23/21.
+//  Created by Katie Till on 2/23/21.
 //  Copyright © 2021 Sam Spohn. All rights reserved.
 //
 
 import XCTest
 @testable import proj4
 
-
 class proj4Tests: XCTestCase {
 
     override func setUpWithError() throws {
         
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
+        super.tearDown()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
+    func testKeys() throws {
+        let testval = 555
+        let sut = Data(from: testval)
+        let status = KeyChain.save(key: "MyNumber", data: sut)
+        let receivedData = KeyChain.load(key:  "MyNumber")
+        let result = receivedData!.to(type: Int.self)
+        XCTAssertEqual(result, 475, "Mismatched key values in keychain test")             // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
