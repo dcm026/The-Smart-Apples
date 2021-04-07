3cad67319708a44c96357f6bf39a1b6ff1da01e1
hawk1000525 <josephaham@hotmail.com>
2021-04-06 20:39:27 -0500

    Login Structs Extracted

--- proj4/LoginScreen.swift
+++ proj4/LoginScreen.swift
@@ -15,37 +15,12 @@ struct LoginScreen : View {
     @State var password: String = ""
     var body: some View {
         VStack {
-            Text("Sign In")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .padding(.bottom, 20)
-                
-            Image("UserImage")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 150, height: 150)
-                        .clipped()
-                        .cornerRadius(150)
-                        .padding(.bottom, 75)
-            TextField("Username", text: $username)
-                        .padding()
-                        .background(lightGreyColor)
-                        .cornerRadius(5.0)
-                        .padding(.bottom, 20)
-            SecureField("Password", text: $password)
-                        .padding()
-                        .background(lightGreyColor)
-                        .cornerRadius(5.0)
-                        .padding(.bottom, 20)
-            
+            SignIn()
+            UserImage()
+            UsernameField(username: $username)
+            PasswordField(password: $password)
             Button(action: {print("Button tapped")}) {
-                Text("LOGIN")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 220, height: 60)
-                        .background(Color.green)
-                        .cornerRadius(15.0)
+                LoginText()
             }
         }
         .padding()
@@ -55,5 +30,60 @@ struct LoginScreen : View {
 struct LoginScreen_Previews: PreviewProvider {
     static var previews: some View {
         LoginScreen()
+    }
+}
+
+struct SignIn: View {
+    var body: some View {
+        Text("Sign In")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image("UserImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct LoginText: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
+    }
+}
+
+struct UsernameField: View {
+    @Binding var username: String
+    var body: some View {
+        TextField("Username", text: $username)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordField: View {
+    @Binding var password: String
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
     }
 }
